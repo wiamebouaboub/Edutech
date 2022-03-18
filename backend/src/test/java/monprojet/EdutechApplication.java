4f4a173e90920c102607e26e2a7eb39e7a2a45ec
wiamebouaboub <wiame.bouaboub@etud.univ-jfc.fr>
2022-03-18 15:25:03 +0100

    nouveau projet avec exemple du prof

---
+++ backend/src/test/java/monprojet/EdutechApplication.java
@@ -0,0 +1,13 @@
+package monprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechApplication.class, args);
+	}
+
+}
